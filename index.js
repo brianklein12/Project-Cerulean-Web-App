f83bd1abf9e69574b4b6feea56af575290a10e6d
blklein <blklein@umich.edu>
2019-07-20 00:11:16 -0500

    changing to static webpage displaying photo

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-app.get('/', (req, res) => {
-  res.send('Project C killing the game!')
-})
+
+app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
+
