67d44d9a10863476ecf7fae3d1a6602942c809f4
blklein <blklein@umich.edu>
2019-07-19 23:41:42 -0500

    setup pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'Project-C',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-3-87-243-214.compute-1.amazonaws.com',
+      key: '~/Downloads/bk_key_pair.pem',
+      ref: 'origin/master',
+      repo: 'git@github.com:brianklein12/Project-Cerulean-Web-App.git',
+      path: '/home/ubuntu/Project-Cerulean',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
