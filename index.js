2e5f239da02100bfc684bfb36e839d0504bedc1c
blklein <blklein@umich.edu>
2019-07-19 22:58:22 -0500

    first commit with basic static express server

---
+++ index.js
@@ -0,0 +1,6 @@
+onst express = require('express')
+const app = express()
+app.get('/', (req, res) => {
+  res.send('Project C killing the game!')
+})
+app.listen(3000, () => console.log('Server running on port 3000'))
