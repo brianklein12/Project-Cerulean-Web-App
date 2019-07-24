c62729b25ef501a44ebd0fcf9970d7e6212a6505
blklein <blklein@umich.edu>
2019-07-23 22:26:19 -0500

    added get to navigate to html doc

--- index.js
+++ index.js
@@ -1,17 +1,28 @@
 const express = require('express')
 const app = express()
+var bodyParser     =         require("body-parser");
 
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
 
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.post('/logTemp',function(req,res){
+  var temp=req.body.Temperature;
+  var humidity=req.body.Humidity;
+  console.log("Temp = "+temp+", humidity is "+humidity);
+  res.end("yes");
+});
+
+app.get('/logTempTest',function(req,res){
+  res.sendfile("htmlBucketForNow/tempLogger.html");
+});
+
 // GET method route
 app.get('/testGetter', function (req, res) {
   res.send('GET request to the homepage')
-})
-
-// POST method route
-app.post('/', function (req, res) {
-  res.send('POST request to the homepage')
 })
 
 app.use(function (req, res, next) {
