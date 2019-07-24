f9abff960603d7284771a75be1ebe61b7a29653c
blklein <blklein@umich.edu>
2019-07-23 21:54:33 -0500

    messing around with post handling

--- index.js
+++ index.js
@@ -4,3 +4,16 @@ const app = express()
 app.use(express.static('public'))
 app.listen(3000, () => console.log('Server running on port 3000'))
 
+// GET method route
+app.get('/', function (req, res) {
+  res.send('GET request to the homepage')
+})
+
+// POST method route
+app.post('/', function (req, res) {
+  res.send('POST request to the homepage')
+})
+
+app.use(function (req, res, next) {
+  res.status(404).send("Sorry can't find that!")
+})
