8bc3cda2d6c989e8d023d2c75877d7231395c71a
blklein <blklein@umich.edu>
2019-07-24 17:07:08 -0500

    using online tutorial

--- index.js
+++ index.js
@@ -1,30 +1,50 @@
-const express = require('express')
-const app = express()
-var bodyParser = require('body-parser');
+// const express = require('express')
+// const app = express()
+// var bodyParser = require('body-parser');
 
-app.use(express.static('public'))
-app.listen(3000, () => console.log('Server running on port 3000'))
+// app.use(express.static('public'))
+// app.listen(3000, () => console.log('Server running on port 3000'))
 
+
+// app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
+
+// app.post('/logTemp',function(req,res){
+//   var temp=req.body.Temperature;
+//   var humidity=req.body.Humidity;
+//   console.log("Temp = "+temp+", humidity is "+humidity);
+//   res.end("yes");
+// });
+
+// app.get('/logTempTest',function(req,res){
+//   res.sendfile("htmlBucketForNow/tempLogger.html");
+// });
+
+// // GET method route
+// app.get('/testGetter', function (req, res) {
+//   res.send('GET request to the homepage')
+// })
+
+// app.use(function (req, res, next) {
+//   res.status(404).send("Sorry can't find that!")
+// })
+
+var express        =         require("express");
+var bodyParser     =         require("body-parser");
+var app            =         express();
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
-app.post('/logTemp',function(req,res){
-  var temp=req.body.Temperature;
-  var humidity=req.body.Humidity;
-  console.log("Temp = "+temp+", humidity is "+humidity);
+app.get('/',function(req,res){
+  res.sendfile("/htmlBucketForNow/tester.html");
+});
+app.post('/login',function(req,res){
+  var user_name=req.body.user;
+  var password=req.body.password;
+  console.log("User name = "+user_name+", password is "+password);
   res.end("yes");
 });
-
-app.get('/logTempTest',function(req,res){
-  res.sendfile("htmlBucketForNow/tempLogger.html");
-});
-
-// GET method route
-app.get('/testGetter', function (req, res) {
-  res.send('GET request to the homepage')
-})
-
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!")
+app.listen(3000,function(){
+  console.log("Started on PORT 3000");
 })
