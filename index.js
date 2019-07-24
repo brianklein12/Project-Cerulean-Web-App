6df29b180041ad17a026a2be9a5128ce3bcf9aa4
blklein <blklein@umich.edu>
2019-07-24 17:18:15 -0500

    done word

--- index.js
+++ index.js
@@ -43,7 +43,7 @@ app.post('/login',function(req,res){
   var user_name=req.body.user;
   var password=req.body.password;
   console.log("User name = "+user_name+", password is "+password);
-  res.end("yes");
+  res.end("done");
 });
 app.listen(3000,function(){
   console.log("Started on PORT 3000");
