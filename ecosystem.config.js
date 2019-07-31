52e2552c8574d20ad39eae6b98d3a7c6cf3486b4
Patrick Cameron <patrick.a.cameron@gmail.com>
2019-07-31 17:49:08 -0500

    made downloads lowercase

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -7,7 +7,7 @@ module.exports = {
     production: {
       user: 'ubuntu',
       host: 'ec2-54-164-130-248.compute-1.amazonaws.com',
-      key: '~/Downloads/bk_key_pair.pem',
+      key: '~/downloads/bk_key_pair.pem',
       ref: 'origin/master',
       repo: 'git@github.com:brianklein12/Project-Cerulean-Web-App.git',
       path: '/home/ubuntu/Project-Cerulean',
