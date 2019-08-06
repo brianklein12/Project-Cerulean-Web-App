d5dfbcb6c73e93f0f2c40aa5e5cb0fa0fe80cfd4
herman <hermanpineda94@gmail.com>
2019-08-05 22:58:49 -0500

    Temp/Humidity script and sensor datasheet

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -6,8 +6,8 @@ module.exports = {
   deploy: {
     production: {
       user: 'ubuntu',
-      host: 'ec2-54-164-130-248.compute-1.amazonaws.com',
-      key: '~/downloads/bk_key_pair.pem',
+      host: 'ec2-54-175-162-19.compute-1.amazonaws.com',
+      key: '/Users/hermanpineda/Documents/bk_key_pair.pem',
       ref: 'origin/master',
       repo: 'git@github.com:brianklein12/Project-Cerulean-Web-App.git',
       path: '/home/ubuntu/Project-Cerulean',
---
+++ raspberryPi/tempHumidity.py
@@ -0,0 +1,32 @@
+# This script uses the Grove SHT31 temperature and humidity sensor.
+#
+# I2C Address: 0x44
+# Voltage:     3.3 V or 5.5 V
+# Temperature Sensor Range: -40–125°C, with ±0.3°C accuracy
+
+import smbus
+import time
+
+# Get I2C bus
+bus = smbus.SMBus(1)
+ 
+# SHT31 address, 0x44(68)
+bus.write_i2c_block_data(0x44, 0x2C, [0x06])
+ 
+time.sleep(0.5)
+ 
+# SHT31 address, 0x44(68)
+# Read data back from 0x00(00), 6 bytes
+# Temp MSB, Temp LSB, Temp CRC, Humididty MSB, Humidity LSB, Humidity CRC
+data = bus.read_i2c_block_data(0x44, 0x00, 6)
+ 
+# Convert data.
+temp     = data[0] * 256 + data[1]
+cTemp    = -45 + (175 * temp / 65535.0)
+fTemp    = -49 + (315 * temp / 65535.0)
+humidity = 100 * (data[3] * 256 + data[4]) / 65535.0
+ 
+# Print data.
+print ("Temperature in Celsius is : %.2f C"    %cTemp)
+print ("Temperature in Fahrenheit is : %.2f F" %fTemp)
+print ("Relative Humidity is : %.2f %%RH"      %humidity)
